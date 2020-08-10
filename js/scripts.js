2dbd941a2896be0ed99a3bb93704eeffc04205c8
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-10 16:41:23 -0700

    Add function to substitute letters to javascript.
    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,5 @@ let formanswer =prompt("Please enter a sentence.");
 return formanswer;
 
 function newString(){
- return formanswer.charAt(0) + formanswer.charAt(formanswer.length-1);
+ return formanswer.charAt(0) + formanswer.charAt(formanswer.length()-1);
 }
